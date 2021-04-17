922487051ce374f50c913630e84ebdab752fbaef
Starry killer <1539871957@qq.com>
2021-04-17 21:28:06 +0800

    Tested on STC12LE5A60S2 and fix picture showing

--- Program/lcdDriver.h
+++ Program/lcdDriver.h
@@ -1,12 +1,16 @@
+#ifndef LCDDRIVER_H
+#define LCDDRIVER_H
+
 #include "RGBConvert.h"
 #include "stc12c5a.h"
 
-sbit cs = P3^5;	// 模拟SPI的CS口
-sbit rs = P2^3;	// 模拟SPI的RS口, 部分屏幕可能为A0(AO)
-sbit sda = P2^2;	// 模拟SPI的SDA口
-sbit scl = P2^1;	// 模拟SPI的SCL口, 部分屏幕可能为SCK
-sbit reset = P3^4;   // RESET口, 可简写为RST
-sbit bl = P1^5;	// 屏幕的背光口, BL或者LED-
+sbit	cs=P2^1;	// 模拟SPI的CS口
+sbit	rs=P2^0; // 模拟SPI的RS口, 部分屏幕可能为A0(AO)
+sbit	sda=P0^1;	// 模拟SPI的SDA口
+sbit	scl=P0^0;	// 模拟SPI的SCL口, 部分屏幕可能为SCK
+sbit	reset=P1^0;   // RESET口, 可简写为RST
+sbit	bl=P0^2;	// 屏幕的背光口, BL或者LED-
+
 // 可直接接3V常亮, 也可以控制
 
 //#define USE_LANDSCAPE 
@@ -25,7 +29,8 @@ sbit bl = P1^5;	// 屏幕的背光口, BL或者LED-
 #define GRAY2 0x4208   
 #define PURPLE 0xb299
 #define SKYBLUE 0x8f7d
-
+char *reverse(char *s);
+char *itoa(int n);
 void output_Pixel(unsigned int x,unsigned int y); // 输出一个像素
 void display_ASCII8x16(unsigned int x0,unsigned int y0,unsigned char *s); // 在指定坐标显示ASCII字符
 void delay_ms(unsigned int time); // 延迟x毫秒
@@ -38,6 +43,12 @@ void LCD_Init(); // 初始化LCD, 必须调用
 void LCD_SetRegion(unsigned int startX, unsigned int startY, unsigned int endX, unsigned int endY); // 设置显示区域，在此区域写数据自动换行
 void putPixel(unsigned int x, unsigned int y, unsigned int color); // 将一个像素设为指定颜色
 void displaySingleColour(int color); // 单色填充屏幕
+void LCD_DrawLine(unsigned int x1, unsigned int y1, unsigned int x2, unsigned int y2, unsigned int color);
+void LCD_ShowPicture(unsigned int x,unsigned int y,unsigned int length,unsigned int width);
+void LCD_DrawRectangle(unsigned int x1, unsigned int y1, unsigned int x2, unsigned int y2,unsigned int color);
+void LCD_DrawCircle(unsigned int x0,unsigned int y0,unsigned int r,unsigned int color);
+void LCD_Fill(unsigned int startX,unsigned int startY,unsigned int endX,unsigned int endY,unsigned int color);
 unsigned char code Zk_ASCII8x16[];
 const unsigned char code picture1[];
 unsigned char code picture2[];
+#endif
--- Program/main.c
+++ Program/main.c
@@ -5,27 +5,36 @@
 #include <string.h>
 
 #include "lcdDriver.h"
+#include "RGBConvert.h"
 
 #define MCU_STC12
+char t[];
+void int0();
+void i1();
+
+char i=0;		
 
 typedef unsigned char uchar;
 typedef unsigned int uint;
 	
-void main() {
+	void main() {
 	#ifdef MCU_STC12
 	P3M1 &= ~(1<<2),
 	P3M0 |=  (1<<2);        //P3.2 set as push-pull output mode
 	#endif
 	LCD_Init();
-	bl=1;
+	bl=~bl; 
+		displaySingleColour(WHITE);
+//	LCD_DrawLine(1, 1, 100, 100, PURPLE);
+//	LCD_DrawLine(1, 100, 100, 1, BLACK);
+		/*
+	for (i=0;i<=10;i++) {
+		LCD_Fill(14,30,14+i,40,BLACK);
+		LCD_Fill(35,80,35+8,80+16,WHITE);
+//		t=itoa(i);
+		display_ASCII8x16(35,80,itoa(i));
+	}*/
+	LCD_ShowPicture(0,0,100,100);
 	while(1) {
-		displaySingleColour(RED);//红色
-		delay_ms(500);
-		displaySingleColour(GREEN);//绿色        
-		delay_ms(500);
-		displaySingleColour(BLUE);//蓝色
-		delay_ms(500);        
-		displaySingleColour(WHITE);//白色
-		delay_ms(500);        
     }
 }
