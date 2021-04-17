90f20dac313660a22d88ef6644e8db68711d3b78
SMLJerry <1539871957@qq.com>
2021-04-17 11:17:49 +0800

    Add LCD Driver.

--- Program/lcdDriver.h
+++ Program/lcdDriver.h
@@ -25,3 +25,19 @@ sbit bl = P1^5;	// 屏幕的背光口, BL或者LED-
 #define GRAY2 0x4208   
 #define PURPLE 0xb299
 #define SKYBLUE 0x8f7d
+
+void output_Pixel(unsigned int x,unsigned int y); // 输出一个像素
+void display_ASCII8x16(unsigned int x0,unsigned int y0,unsigned char *s); // 在指定坐标显示ASCII字符
+void delay_ms(unsigned int time); // 延迟x毫秒
+void SPI_WriteData(unsigned char Data); // 向SPI总线传输一个8位数据
+void LCD_WriteIndex(unsigned char Data); // 向屏幕写入一个8位指令
+void LCD_WriteData(unsigned char Data); // 向屏幕写入一个8位数据
+void LCD_WriteData_16Bit(unsigned int Data); // 向屏幕写入一个16位数据
+void LCD_Reset(); // 重置屏幕
+void LCD_init(); // 初始化LCD, 必须调用
+void LCD_SetRegion(unsigned int startX, unsigned int startY, unsigned int endX, unsigned int endY); // 设置显示区域，在此区域写数据自动换行
+void putPixel(unsigned int x, unsigned int y, unsigned int color); // 将一个像素设为指定颜色
+void displaySingleColour(int color); // 单色填充屏幕
+unsigned char code Zk_ASCII8X16[];
+unsigned char code picture1[];
+unsigned char code picture2[];
